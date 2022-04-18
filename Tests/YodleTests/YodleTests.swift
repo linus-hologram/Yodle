c4248fed634e9ed574f264ca54feff6b562eca4b
Linus Bohle <linus.bohle@icloud.com>
2022-04-18 23:12:41 +0200

    initial package commit

---
+++ Tests/YodleTests/YodleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Yodle
+
+final class YodleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Yodle().text, "Hello, World!")
+    }
+}
