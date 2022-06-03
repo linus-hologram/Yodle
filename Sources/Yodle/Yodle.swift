7472f19cd0e7a1f037199864b3589cefb244e6cc
Linus Bohle <linus.bohle@icloud.com>
2022-06-03 13:58:58 +0200

    added ssl handlers to connect process

--- Sources/Yodle/Yodle.swift
+++ Sources/Yodle/Yodle.swift
@@ -1,18 +1,62 @@
 import NIO
 import NIOPosix
+import NIOSSL
+
+public enum YodleSSLOption {
+    case insecure
+    case TLS(YodleSSLConfiguration)
+    case STARTTLS(YodleSSLConfiguration)
+}
+
+public enum YodleSSLConfiguration {
+    case standard
+    case custom(TLSConfiguration)
+    
+    internal func makeTLSConfiguration() -> TLSConfiguration {
+        switch self {
+        case .standard:
+            return TLSConfiguration.makeClientConfiguration()
+        case .custom(let configuration):
+            return configuration
+        }
+    }
+}
+
+internal struct ClientHandshake {
+    init?() async throws {
+        // perform EHLO procedure
+        return nil
+    }
+}
 
 public struct Yodle {
-    public static func connect(eventLoop: EventLoop, hostname: String, port: Int) async throws -> YodleClient {
+    public static func connect(eventLoop: EventLoop, hostname: String, port: Int, ssl: YodleSSLOption) async throws -> YodleClient {
         var yodleContext: YodleClientContext?
         
-        return try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<YodleClient, Error>) in
+        let client = try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<YodleClient, Error>) in
             ClientBootstrap(group: eventLoop).channelInitializer { channel in
                 yodleContext = YodleClientContext(eventLoop: eventLoop, channel: channel)
                 
                 let yodleDecoder = ByteToMessageHandler(YodleInboundHandler(context: yodleContext!))
                 let yodleSerializer = MessageToByteHandler(YodleOutboundHandler())
                 
-                return channel.pipeline.addHandlers([yodleDecoder, yodleSerializer])
+                var handlers: [ChannelHandler] = [yodleDecoder, yodleSerializer]
+                
+                switch ssl {
+                case .insecure, .STARTTLS(_):
+                    break
+                case .TLS(let yodleSSLConfiguration):
+                    do {
+                        let sslContext = try NIOSSLContext(configuration: yodleSSLConfiguration.makeTLSConfiguration())
+                        let sslClientHandler = try NIOSSLClientHandler(context: sslContext, serverHostname: hostname)
+                        
+                        handlers.insert(sslClientHandler, at: 0)
+                    } catch {
+                        return eventLoop.makeFailedFuture(error)
+                    }
+                }
+                
+                return channel.pipeline.addHandlers(handlers)
             }.connect(host: hostname, port: port).whenComplete { result in
                 do {
                     continuation.resume(returning: YodleClient(eventLoop: eventLoop, channel: try result.get(), context: yodleContext!))
@@ -21,6 +65,12 @@ public struct Yodle {
                 }
             }
         })
+        
+//        if case .STARTTLS(let configuration) = ssl {
+//
+//        }
+        
+        return client
     }
 }
 
