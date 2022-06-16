6f271aed7301f0ecf5e781bf0cde5acc35db07a1
Linus Bohle <linus.bohle@icloud.com>
2022-06-16 23:07:57 +0200

    added encoding methods for mime body parts.

--- Sources/Yodle/YodleMIME.swift
+++ Sources/Yodle/YodleMIME.swift
@@ -12,20 +12,44 @@ struct MIMEMultipartContainer {
 }
 
 struct MIMEBodyPart { // default struct for all MIME body parts
-    let contentType: String
-    let charset: String
-    
     let data: Data
     
-    private(set) var customHeaders: [String: String]
+    private(set) var headers: [String: String]
     
-    mutating func declareHeaders(@CustomHeaderBuilder headers: () -> [String: String]) {
-        customHeaders = headers()
+    private var encodedHeaderValues: String {
+        headers.map { "\($0): \($1)\r\n" }.joined()
     }
     
-    func generateBodyData() -> Data {
-        // MARK: - process data to match the MIME standard
-        return data
+    mutating func declareHeaders(@CustomHeaderBuilder headers: () -> [String: String]) {
+        self.headers = headers()
+    }
+    
+    // https://www.rfc-editor.org/rfc/rfc2045#section-6.8, https://docs.microsoft.com/en-us/previous-versions/office/developer/exchange-server-2010/aa494254(v=exchg.140)
+    mutating func encodeToBase64() -> String {
+        // make sure that content transfer encoding is correct, and override if necessary
+        self.headers.merge(["Content-Transfer-Encoding": "base64"]) { _, new in new }
+        
+        var outputString: String = ""
+
+        outputString.append(encodedHeaderValues)
+        outputString.append("\r\n")
+        outputString.append(data.base64EncodedString(options: [.lineLength76Characters, .endLineWithCarriageReturn, .endLineWithLineFeed]))
+        
+        return outputString
+    }
+    
+    // https://www.rfc-editor.org/rfc/rfc2045#section-6.7
+    mutating func encodeToQuotedPrintable() -> String {
+        self.headers.merge(["Content-Transfer-Encoding": "Quoted-Printable"]) { _, new in new }
+        
+        var outputString: String = ""
+
+        outputString.append(encodedHeaderValues)
+        outputString.append("\r\n")
+        
+        // TODO: perform actual encoding
+        
+        return outputString
     }
 }
 
