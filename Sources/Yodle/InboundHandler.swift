bf4a8fb8276672db6a0024de3a048c714144417a
Linus Bohle <linus.bohle@icloud.com>
2022-06-20 19:43:58 +0200

    updates to the channel inbound handler

--- Sources/Yodle/InboundHandler.swift
+++ Sources/Yodle/InboundHandler.swift
@@ -18,54 +18,60 @@ class YodleInboundHandler: ByteToMessageDecoder {
     }
     
     func decode(context: ChannelHandlerContext, buffer: inout ByteBuffer) throws -> DecodingState {
+        if buffer.readableBytesView.firstIndex(of: 0x0A) == nil { return .needMoreData } // need more data if there is no LF occuring in the byte buffer
         
-        var responses: [SMTPResponse] = []
-        
-        while buffer.readableBytes > 0 {
-            do {
-                responses.append(try processRawSMTPResponse(buffer: &buffer))
-            } catch {
-                Task { await yodleContext.disconnect() }
-                throw error
-            }
+        do {
+            let response = try processRawSMTPResponse(buffer: &buffer)
+            Task { await yodleContext.receiveOne(response: response) }
+        } catch {
+            Task { await yodleContext.disconnect() }
+            throw error
         }
-        
-        let _responses = responses
-        
-        Task { await yodleContext.receive(responses: _responses) }
         
         return .continue
         
     }
     
+    func decodeLast(context: ChannelHandlerContext, buffer: inout ByteBuffer, seenEOF: Bool) throws -> DecodingState {
+        if buffer.readableBytes > 0 && buffer.readableBytesView.firstIndex(of: 0x0A) == nil {
+            throw YodleError.InvalidResponseMessage(buffer.readString(length: buffer.readableBytes))
+        } // throw error if the buffer isn't empty and doesn't contain a LF occurrence
+
+        do {
+            let response = try processRawSMTPResponse(buffer: &buffer)
+            Task { await yodleContext.receiveOne(response: response) }
+        } catch {
+            Task { await yodleContext.disconnect() }
+            throw error
+        }
+        
+        return .continue
+    }
+    
     func processRawSMTPResponse(buffer: inout ByteBuffer) throws -> SMTPResponse {
         guard let codeDigits = buffer.readString(length: 3) else {
-            throw YodleError.InvalidResponseCode(nil)
+            throw YodleError.UnexpectedError("Inbound byte buffer insufficiently propagated. This should not have happened.")
         }
         
         guard let code = Int(codeDigits) else {
             throw YodleError.InvalidResponseCode(codeDigits)
         }
         
-        guard buffer.readableBytes >= 2 else { throw YodleError.InvalidResponseMessage(nil) } // even if there's no text, \r\n is still needed
-        
-        var message: String = ""
-        
-        while !message.hasSuffix("\r\n") {
-            guard let char = buffer.readString(length: 1) else {
-                throw YodleError.InvalidResponseMessage(message)
-            }
-            
-            message.append(char)
+        guard let crIndex = buffer.readableBytesView.firstIndex(of: 0x0D),
+              let lfIndex = buffer.readableBytesView.firstIndex(of: 0x0A),
+              lfIndex - crIndex == 1, // they must follow after another
+              lfIndex + 3 <= 511 // stop after 512 characters including CRLF and the 3 digits in the beginning; https://datatracker.ietf.org/doc/html/rfc5321#section-4.5.3.1.5
+        else {
+            throw YodleError.InvalidResponseMessage(buffer.readString(length: buffer.readableBytes))
         }
         
-        if message.first == " " {
+        guard var message = buffer.readString(length: crIndex) else { throw YodleError.UnexpectedError("Could not read string from byte buffer. This should not have happened.")}
+        buffer.moveReaderIndex(forwardBy: 2) // skip the CRLF sequence
+        
+        if message.first == " " || message.first == "-" {
             message.removeFirst()
         }
-        
-        message.removeLast(2) // remove the \r\n
-        
+                
         return SMTPResponse(code: code, message: message)
     }
-    
 }
